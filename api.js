26c33db818b62499a382e45feab8c9bc648dea01
Rory <roryodonnell97@gmail.com>
2025-07-16 19:02:18 +0100

    Added current location functionality

--- api.js
+++ api.js
@@ -4,11 +4,26 @@ const API_KEY = '377353b2705d73d27295030e6fa3cfd1'
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
 const getWeather = async (location) => {
-  const params = {
-    q: location,
-    units: 'imperial',
-    appid: API_KEY,
-  };
+  let params;
+
+  // Check if location is a string (location name) or an object (latitude/longitude)
+  if (typeof location === 'string') {
+    params = {
+      q: location,
+      units: 'imperial',
+      appid: API_KEY,
+    };
+  } else if (typeof location === 'object' && location.lat && location.lon) {
+    params = {
+      lat: location.lat,
+      lon: location.lon,
+      units: 'imperial',
+      appid: API_KEY,
+    };
+  } else {
+    throw new Error('Invalid location format. Please use a string for location name or an object with lat and lon properties for latitude/longitude.');
+  }
+
   const response = await axios.get(BASE_URL, { params });
   return response.data;
 };
--- screens/CurrentLocationWeatherScreen.js
+++ screens/CurrentLocationWeatherScreen.js
@@ -1,11 +1,57 @@
-// SettingsScreen.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import * as Location from 'expo-location';
+import getWeather from '../api.js';
 
 const CurrentLocationWeatherScreen = () => {
+  const [weatherData, setWeatherData] = useState(null);
+  const [location, setLocation] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+          setError('Permission to access location was denied');
+          return;
+        }
+
+        const location = await Location.getCurrentPositionAsync({});
+        setLocation(location);
+
+        console.log('Location:', location);
+
+        // Pass location object to getWeather function
+        const weatherData = await getWeather({ lat: location.coords.latitude, lon: location.coords.longitude });
+        console.log('Weather Data:', weatherData);
+        setWeatherData(weatherData);
+      } catch (error) {
+        console.error('Error:', error);
+        setError(error.message);
+      }
+    })();
+  }, []);
+
+  if (error) {
+    return (
+      <View>
+        <Text>Error: {error}</Text>
+      </View>
+    );
+  }
+
+  if (!weatherData) {
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
   return (
     <View>
-      <Text>Current Location Weather Screen</Text>
+      <Text>Temperature: {weatherData.main.temp}°F</Text>
     </View>
   );
 };
