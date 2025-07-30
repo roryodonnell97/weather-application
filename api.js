b5684a615d02e0c09de9cad447933a6e9e074601
Rory <roryodonnell97@gmail.com>
2025-07-30 17:48:45 +0100

    Moved checkLocationType to weatherUtils

--- api.js
+++ api.js
@@ -1,30 +1,12 @@
 import axios from 'axios/dist/axios.min';
+import { checkLocationType } from './weatherUtils.js';
 
-const API_KEY = '377353b2705d73d27295030e6fa3cfd1'
-const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
+const CURRENT_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
 const getWeather = async (location) => {
-  let params;
+  const locationType  = checkLocationType(location);
 
-  // Check if location is a string (location name) or an object (latitude/longitude)
-  if (typeof location === 'string') {
-    params = {
-      q: location,
-      units: 'imperial',
-      appid: API_KEY,
-    };
-  } else if (typeof location === 'object' && location.lat && location.lon) {
-    params = {
-      lat: location.lat,
-      lon: location.lon,
-      units: 'imperial',
-      appid: API_KEY,
-    };
-  } else {
-    throw new Error('Invalid location format. Please use a string for location name or an object with lat and lon properties for latitude/longitude.');
-  }
-
-  const response = await axios.get(BASE_URL, { params });
+  const response = await axios.get(CURRENT_WEATHER_URL, { params: locationType.params });
   return response.data;
 };
 
--- weatherUtils.js
+++ weatherUtils.js
@@ -5,4 +5,32 @@ const getWindDirection = (degrees) => {
   return directions[index];
 };
 
-export {  getWindDirection};
+// Check if location is a string (location name) or an object (latitude/longitude)
+const checkLocationType = (location) => {
+const API_KEY = '377353b2705d73d27295030e6fa3cfd1'
+
+  if (typeof location === 'string') {
+    return {
+      type: 'string',
+      params: {
+        q: location,
+        units: 'imperial',
+        appid: API_KEY,
+      },
+    };
+  } else if (typeof location === 'object' && location.lat && location.lon) {
+    return {
+      type: 'object',
+      params: {
+        lat: location.lat,
+        lon: location.lon,
+        units: 'imperial',
+        appid: API_KEY,
+      },
+    };
+  } else {
+    throw new Error('Invalid location format. Please use a string for location name or an object with lat and lon properties for latitude/longitude.');
+  }
+};
+
+export { getWindDirection, checkLocationType };
